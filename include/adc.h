dfef5ef5b4ada7374c97b5a7a497fa6cc1899477
daebangstn <gunho1107@naver.com>
2024-03-05 12:30:27 +0900

    real foot test

--- include/adc.h
+++ include/adc.h
@@ -4,10 +4,21 @@
 #define PIN_NUM_MISO      19
 #define PIN_NUM_MOSI      23
 #define PIN_NUM_CLK       18
-#define PIN_NUM_CS        5
-#define PIN_NUM_DRDY      10
 
-#define SPI_BAUDRATE      1000000
+#define PIN_NUM_CS1       32
+#define PIN_NUM_DRDY1     35
+#define PIN_NUM_CS2       21
+#define PIN_NUM_DRDY2     22
+#define PIN_NUM_CS3       27
+#define PIN_NUM_DRDY3     14
+#define PIN_NUM_CS4       16
+#define PIN_NUM_DRDY4     17
+#define PIN_NUM_CS5       10
+#define PIN_NUM_DRDY5     9
+#define PIN_NUM_CS6       2
+#define PIN_NUM_DRDY6     15
+
+#define SPI_BAUDRATE      2000000
 #define SPI_BUS           SPI2_HOST
 /*
     to change this value, 
@@ -18,5 +29,9 @@
 
 #define ADC_DRDY_TIMEOUT_US  100000
 
+#define NUM_ADCS          1
+#define TOTAL_ADCS        6
+#define CHECKED_PIN       2
+
 void adc_task(void *pvParameter);
-esp_err_t write_adc_data(char* buf, float* data, uint8_t* remain_size);
+esp_err_t write_adc_data(char* buf, uint8_t idx_adc, float* data, uint8_t* remain_size);
--- src/adc.c
+++ src/adc.c
@@ -37,14 +37,25 @@ static const uint8_t resolution = 15;
 static unsigned char rx_data[4];
 volatile BaseType_t sem_block_drdy = pdFALSE;
 
+struct ADC_PINS
+{
+    uint8_t cs[TOTAL_ADCS];
+    uint8_t drdy[TOTAL_ADCS];
+};
+
+struct ADC_PINS adc_pins = {
+    .cs = {PIN_NUM_CS1, PIN_NUM_CS2, PIN_NUM_CS3, PIN_NUM_CS4, PIN_NUM_CS5, PIN_NUM_CS6},
+    .drdy = {PIN_NUM_DRDY1, PIN_NUM_DRDY2, PIN_NUM_DRDY3, PIN_NUM_DRDY4, PIN_NUM_DRDY5, PIN_NUM_DRDY6},
+};
+
 void init_spi(void);
 spi_device_handle_t init_device(void);
-void read_adc(spi_device_handle_t spi, uint8_t resolution, float* voltages);
+void read_adc(uint8_t idx_adc, spi_device_handle_t spi, uint8_t resolution, float* voltages);
 spi_transaction_t get_transaction(uint8_t num_ain);
 float convert_voltage(spi_transaction_t t);
 esp_err_t check_echoed_tx(spi_transaction_t t);
-void pre_spi_read(spi_transaction_t* tx);
-void post_spi_read(spi_transaction_t* tx, float* v);
+void pre_spi_read(uint8_t idx_adc, spi_transaction_t* tx);
+void post_spi_read(uint8_t idx_adc, spi_transaction_t* tx, float* v);
 
 void adc_task(void *pvParameter) {
     init_spi();
@@ -55,26 +66,27 @@ void adc_task(void *pvParameter) {
 
     while (true) {
         uint8_t remain_size = SPP_DATA_LEN;
-        read_adc(spi, 16, voltages);
-        ESP_LOGI(TAG, "voltages: 0/%f, 1/%f, 2/%f, 3/%f", voltages[0], voltages[1], voltages[2], voltages[3]);
-        if (bt_fd == -1) {
-            ESP_LOGW(TAG, "bt is not connected");
-            vTaskDelay(300 / portTICK_PERIOD_MS);
-        } else {
-            ESP_ERROR_CHECK(write_time_data(bt_data + (buf_size - remain_size), &remain_size));
-            ESP_ERROR_CHECK(write_adc_data(bt_data + (buf_size - remain_size), voltages, &remain_size));
-            ESP_LOGD(TAG, "remaining write buffer size = %d", remain_size);
-            int size = write(bt_fd, bt_data, SPP_DATA_LEN);
-            if (size == -1) {
-                ESP_LOGE(TAG, "write failed");
-                break;
-            } else if (size == 0) {
-                ESP_LOGW(TAG, "write failed due to ringbuf is full, dropping data");
+        for(int adc_idx=CHECKED_PIN; adc_idx<(NUM_ADCS+CHECKED_PIN); adc_idx++) {
+            read_adc(adc_idx, spi, resolution, voltages);
+            ESP_LOGI(TAG, "idx:%d, voltages: 0/%f, 1/%f, 2/%f, 3/%f", adc_idx, voltages[0], voltages[1], voltages[2], voltages[3]);
+            if (bt_fd == -1) {
+                ESP_LOGW(TAG, "bt is not connected");
+                vTaskDelay(300 / portTICK_PERIOD_MS);
             } else {
-                ESP_LOGI(TAG, "fd = %d  data_len = %d", bt_fd, size);
+                ESP_ERROR_CHECK(write_time_data(bt_data + (buf_size - remain_size), &remain_size));
+                ESP_ERROR_CHECK(write_adc_data(bt_data + (buf_size - remain_size), adc_idx, voltages, &remain_size));
+                ESP_LOGD(TAG, "remaining write buffer size = %d", remain_size);
+                int size = write(bt_fd, bt_data, SPP_DATA_LEN);
+                if (size == -1) {
+                    ESP_LOGE(TAG, "write failed");
+                    break;
+                } else if (size == 0) {
+                    ESP_LOGW(TAG, "write failed due to ringbuf is full, dropping data");
+                } else {
+                    ESP_LOGI(TAG, "fd = %d  data_len = %d", bt_fd, size);
+                }
             }
         }
-
         vTaskDelay(50 / portTICK_PERIOD_MS);
     }
 }
@@ -109,30 +121,32 @@ spi_device_handle_t init_device(void) {
     ESP_ERROR_CHECK(spi_bus_add_device(SPI_BUS, &dev_config, &spi));
     ESP_ERROR_CHECK(spi_device_acquire_bus(spi, portMAX_DELAY));
     
-    gpio_set_level(PIN_NUM_CS, 0);
-    gpio_config_t cs_cfg = {
-        .pin_bit_mask = BIT64(PIN_NUM_CS),
-        .mode = GPIO_MODE_OUTPUT,
-    };
-    gpio_config(&cs_cfg);
-    gpio_config_t drdy_cfg = {
-        .pin_bit_mask = BIT64(PIN_NUM_DRDY),
-        .pull_up_en = GPIO_PULLUP_ENABLE,
-        .mode = GPIO_MODE_INPUT,
-    };
-    gpio_config(&drdy_cfg);
+    for (int i=0; i<TOTAL_ADCS; i++) {
+        gpio_set_level(adc_pins.cs[i], 1);
+        gpio_config_t cs_cfg = {
+            .pin_bit_mask = BIT64(adc_pins.cs[i]),
+            .mode = GPIO_MODE_OUTPUT,
+        };
+        gpio_config(&cs_cfg);
+        gpio_config_t drdy_cfg = {
+            .pin_bit_mask = BIT64(adc_pins.drdy[i]),
+            .pull_up_en = GPIO_PULLUP_ENABLE,
+            .mode = GPIO_MODE_INPUT,
+        };
+        gpio_config(&drdy_cfg);
+    }
     return spi;
 }
 
-void pre_spi_read(spi_transaction_t* tx) {
+void pre_spi_read(uint8_t idx_adc, spi_transaction_t* tx) {
     for(int i=0; i<6; i++) {
         tx[i] = get_transaction(i);
     }
-    gpio_set_level(PIN_NUM_CS, 0);
+    gpio_set_level(adc_pins.cs[idx_adc], 0);
 }
 
-void post_spi_read(spi_transaction_t* tx, float* v) {
-    gpio_set_level(PIN_NUM_CS, 1);
+void post_spi_read(uint8_t idx_adc, spi_transaction_t* tx, float* v) {
+    gpio_set_level(adc_pins.cs[idx_adc], 1);
     for(int i=2; i<6; i++) {
         if(check_echoed_tx(tx[i]) != ESP_OK) {
             ESP_LOGE(TAG, "returned register values do not match sent values");
@@ -146,12 +160,12 @@ void post_spi_read(spi_transaction_t* tx, float* v) {
     }
 }
 
-void read_adc(spi_device_handle_t spi, uint8_t resolution, float* voltages) {
+void read_adc(uint8_t idx_adc, spi_device_handle_t spi, uint8_t resolution, float* voltages) {
     spi_transaction_t tx[6];
-    pre_spi_read(tx);
+    pre_spi_read(idx_adc, tx);
     for (uint8_t i=0; i<6; i++) {
         int64_t now = esp_timer_get_time();
-        while (gpio_get_level(PIN_NUM_DRDY) == 1) {
+        while (gpio_get_level(adc_pins.drdy[idx_adc]) == 1) {
             if (esp_timer_get_time() - now > ADC_DRDY_TIMEOUT_US) {
                 ESP_LOGE(TAG, "timeout waiting for drdy");
                 break;
@@ -159,7 +173,7 @@ void read_adc(spi_device_handle_t spi, uint8_t resolution, float* voltages) {
         }
         ESP_ERROR_CHECK(spi_device_polling_transmit(spi, &tx[i]));
     }
-    post_spi_read(tx, voltages);
+    post_spi_read(idx_adc, tx, voltages);
 }
 
 spi_transaction_t get_transaction(uint8_t num_ain) {
@@ -215,10 +229,10 @@ esp_err_t check_echoed_tx(spi_transaction_t t) {
 }
 
 // data must be float[4]
-esp_err_t write_adc_data(char* buf, float* data, uint8_t* remain_size) {
+esp_err_t write_adc_data(char* buf, uint8_t idx_adc, float* data, uint8_t* remain_size) {
     uint8_t buf_size = *remain_size;
     for (int i=0; i<4; i++) {
-        int res = snprintf(buf, buf_size, "%d:%4.2f/", i, data[i]);
+        int res = snprintf(buf, buf_size, "%d-%d:%4.2f/", idx_adc, i, data[i]);
         if (res >= *remain_size || res < 0) {
             ESP_LOGE(TAG, "snprintf failed buf_size: %d res: %d", *remain_size, res);
             return ESP_FAIL;
